11dcce7b4980d585cd1a923e2ada6b825acb6eaa
HZ <zhaihaohk@gmail.com>
2022-06-28 22:43:10 +1200

    Draft version

---
+++ src/metrics/event.listener.ts
@@ -0,0 +1,26 @@
+// Copyright 2020-2022 SubQuery Pte Ltd authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { OnEvent } from '@nestjs/event-emitter';
+import { InjectMetric } from '@willsoto/nestjs-prometheus';
+import { Gauge } from 'prom-client';
+import { EventPayload, metric, ServiceEvent } from './events';
+
+export class MetricEventListener {
+  constructor(
+    @InjectMetric(metric(ServiceEvent.CoordinatorVersion))
+    private coordinatorVersion: Gauge<string>,
+    @InjectMetric(metric(ServiceEvent.ControllerBalance))
+    private controllreBalance: Gauge<string>,
+  ) {}
+
+  @OnEvent(ServiceEvent.CoordinatorVersion)
+  handleCoordinatorVersion({ value }: EventPayload<number>) {
+    this.coordinatorVersion.set(value);
+  }
+
+  @OnEvent(ServiceEvent.ControllerBalance)
+  handlerControllerBalance({ value }: EventPayload<number>) {
+    this.controllreBalance.set(value);
+  }
+}
--- src/metrics/metrics.module.ts
+++ src/metrics/metrics.module.ts
@@ -2,15 +2,24 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { Module } from '@nestjs/common';
+import { makeGaugeProvider, PrometheusModule } from '@willsoto/nestjs-prometheus';
 
 import { AccountModule } from 'src/account/account.module';
 import { ServicesModule } from 'src/services/services.module';
 import { PublicMetricsService } from './public.metric.service';
-import { UserMetricsService } from './user.metric.service';
+import { metric, ServiceEvent } from './events';
+import { MetricEventListener } from './event.listener';
 
 @Module({
-  imports: [AccountModule, ServicesModule],
-  providers: [PublicMetricsService, UserMetricsService],
-  exports: [PublicMetricsService, UserMetricsService],
+  imports: [PrometheusModule.register(), AccountModule, ServicesModule],
+  providers: [
+    MetricEventListener,
+    makeGaugeProvider({
+      name: metric(ServiceEvent.CoordinatorVersion),
+      help: 'The coordiantor service version',
+    }),
+    PublicMetricsService,
+  ],
+  exports: [PublicMetricsService],
 })
 export class MetricsModule { }
